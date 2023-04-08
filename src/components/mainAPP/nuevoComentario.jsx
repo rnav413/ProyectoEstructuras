08785026eda33f9b43bd632ea54a570fe7897064
zarrok <anakinvv@gmail.com>
2023-04-08 17:04:58 -0500

    estilos

--- src/components/mainAPP/nuevoComentario.jsx
+++ src/components/mainAPP/nuevoComentario.jsx
@@ -3,19 +3,25 @@ import './main.css';
 
 export const NuevoComentario = () => {
     return (
-        <div className="post-container">
-  <div className="post">
-    <div className="post-header">
-      <img
-        className="post-profile-image"
-        src="https://via.placeholder.com/50"
-        alt="Profile"
-      />
-      <span className="post-username">Nombre de usuario</span>
-    </div>
-    <textarea className="post-textarea" placeholder="¿Qué estás pensando?" />
-    <button className="post-comment-button">Comentar</button>
-  </div>
-</div>
+        <div className='comments'>
+            <div className="post-container">
+                <div className="post">
+                    <div className="post-header">
+                    <img
+                        className="post-profile-image"
+                        src="https://via.placeholder.com/50"
+                        alt="Profile"
+                    />
+                    <span className="post-username">Nombre de usuario</span>
+                    </div>
+                    <div>
+                        <textarea className="post-textarea" placeholder="¿Qué estás pensando?" />
+                    </div>
+                    <div>
+                        <button className="post-comment-button">Comentar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
       );
 }
