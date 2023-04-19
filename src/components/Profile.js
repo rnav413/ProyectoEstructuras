6b86ee4f9ce95c1fad416343bf9a1417eea12497
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-04-19 05:34:10 -0500

    Perfil - contenedores y carta
    Se agregó la carta del perfil junto con los contenedores correspondientes a la lista de amigos y publicaciones

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,6 +12,13 @@ function ProfilePage() {
             <Link to="/perfil" id='logo-perfil'><img src= {logo_perfil} id='logo-nav' className='filter-green'/></Link>
       </nav>
       <div className='perfil-body'>
+        
+        <div className='perfil-carta'>
+        <img src= {logo_perfil} id='logo-carta' className='filter-green'/>
+        <h1>Nombre</h1>
+        <p>Especie</p>
+        <h2>Genero Tipo  Origen </h2>
+        </div>
         <div className='perfil-container'>
         <div className='perfil-amigos'></div>
         <div className='perfil-publicaciones'></div>
