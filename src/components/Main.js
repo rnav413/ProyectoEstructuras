3ea77aaad8e015bf7734dd771813e9cab69a3c05
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-05-27 20:40:35 -0500

    Nav component
    se creó un componente aparte para la nav bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Main.css";
 import {NuevoComentario,MainScroll,Post} from "./mainAPP";
-import logo_nav from "../assets/SVG/Recurso 3.svg";
-import logo_perfil from "../assets/SVG/Recurso 1.svg";
 import  {UserFetchcharacter} from "../hooks"
+import Nav from './Nav'
 
 
 function MainPage() {
   return (
     <div className="main-page">
-      <nav className='nav_rick_and_morty'>
-          <Link to="/main"><img src= {logo_nav} id='logo-nav'/></Link>
-          <Link to="/perfil" id='logo-perfil'><img src= {logo_perfil} id='logo-nav' className='filter-green'/></Link>
-      </nav>
-      <div className='content'>
-        <MainScroll/>  
+    <Nav />
+    <div className='content'>
+      <MainScroll/>  
     </div>
-    
-     
-    </div>
+  </div>
   );
 }
 
