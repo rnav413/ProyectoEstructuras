db2860d70d60d914fafb157e17e346dd504987a3
rnav413 <69471412+rnav413@users.noreply.github.com>
2023-05-30 23:22:35 -0500

    final

--- src/components/mainAPP/mainAPPScroll.jsx
+++ src/components/mainAPP/mainAPPScroll.jsx
@@ -9,23 +9,61 @@ import { UserContext } from '../../UserContext';
 
 export const MainScroll = () => {
   const [items, setItems] = useState([]);
-  const [hasMore, setHasMore] = useState(true);
   const [newCommentText, setNewCommentText] = useState('');
-  const {characters,isLoading} = UserFetchcharacter();
   const {convertCharacterToArray} = useRickParse()
   const { userData } = useContext(UserContext)
-
+  const [friends, setFriends] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios.post('https://apiestructuras-production.up.railway.app/api/social/getPublicacionesPorId', null ,{headers: {'x-token': userData.token}})
       .then(response => {
-        console.log('publicaciones',response.data);
+        console.log('publicaciones',response.data.publicaciones);
+        setItems(response.data.publicaciones.sort((a, b) => new Date(b.fecha) - new Date(a.fecha)));
       })
       .catch(error => {
         console.error('error en cargar publicaciones',error);
       });
   }, []);
 
+  useEffect(() => {
+   
+    axios.post('https://apiestructuras-production.up.railway.app/api/social/getFriends',null, {headers: { 'x-token': userData.token }})
+      .then(response => {
+        const amigos_id = response.data.friends.map(({ friend }) => friend._id);
+        setFriends(amigos_id);
+        setIsLoading(false);
+        console.log('akkk', amigos_id)
+      }).catch(error => {
+        console.error('Error al obtener los personajes:', error);
+        console.error('Request', error.config)
+      });
+  }, []);
+
+  
+
+  useEffect(() => {
+    friends.forEach(dato => {
+      axios.post('https://apiestructuras-production.up.railway.app/api/social/getPublicacionesPorId', dato, { headers: { 'x-token': userData.token } })
+        .then(response => {
+
+          const nuevosItems = response.data.publicaciones;
+
+          // Agregar los nuevos datos a items
+          setItems(prevItems => [...prevItems, ...nuevosItems]);
+
+          // Ordenar items después de agregar los nuevos datos
+          setItems(prevItems => prevItems.sort((a, b) => new Date(b.fecha) - new Date(a.fecha)));
+        })
+        .catch(error => {
+          // Manejar errores de las solicitudes
+          console.error('Error en la solicitud:', error);
+        });
+    });
+  }, [friends]);
+
+  
+  
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -44,12 +82,9 @@ export const MainScroll = () => {
           <Container/>
 
           {items.map((item) => (
-            <Post key={item.id} comentario={item.texto} />
+            <Post key={item.id} comentario={item.description} name={userData.name} likes={item.likes} dislikes={item.dislikes}/>
           ))}
 
-          {convertCharacterToArray(characters).map((character) => (
-            <Post key={character.id} name={character.name}  image= {character.image} comentario={character.name} />
-          ))}
           
         </InfiniteScroll>
       </div>
--- src/components/mainAPP/reacciones.jsx
+++ src/components/mainAPP/reacciones.jsx
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 
-export const Reacciones = () => {
-  const [count1, setCount1] = useState(Math.floor(Math.random() * 100));
-  const [count2, setCount2] = useState(Math.floor(Math.random() * 100));
+export const Reacciones = (props) => {
+  const [likes, setLikes] = useState(props.likes);
+  const [dislikes, setDislikes] = useState(props.dislikes);
 
   const handleIncrement1 = () => {
-    setCount1(count1 + 1);
+    setLikes(likes + 1);
   };
 
   const handleIncrement2 = () => {
-    setCount2(count2 + 1);
+    setDislikes(dislikes + 1);
   };
 
   return (
     <div className='reacciones'>
       
       <button className='like-button' onClick={handleIncrement1}/>
-      <label className='label_like_count'>{count1}</label>
+      <label className='label_like_count'>{likes}</label>
 
       <br />
       
       <button className="post-dislike-button" onClick={handleIncrement2}/>
-      <label className='label_dislike_count'>{count2}</label>
+      <label className='label_dislike_count'>{dislikes}</label>
 
     </div>
 
